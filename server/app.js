cd99def5ccba64a0e0e419f4db8c85d8ac97489a
Brenden Oliver <Brenden Oliver>
2016-06-03 11:17:00 -0500

    Updated with Bootstrap

--- server/app.js
+++ server/app.js
@@ -7,6 +7,8 @@ let mongoUtility = require("./mongoUtility");
 mongoUtility.connect();
 
 app.use( express.static( __dirname + '/../') );
+app.use('/js', express.static(__dirname + '/../node_modules/bootstrap/dist/js'));
+app.use('/css', express.static(__dirname + '/../node_modules/bootstrap/dist/css'));
 
 let bodyParser = require("body-parser");
 let jsonParser = bodyParser.json();
